6898dc36e7d0b83c38bc00a9625ae2cbefb8fe88
pascalotieno <otieno2john70@gmail.com>
2023-01-09 04:22:04 -0800

    task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
+
 #!/usr/bin/python3
 # 4-inherits_from.py
 """Defines an inherited class-checking function."""
+
 
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
